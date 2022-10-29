ffca0d2adc64c2896712da62ea7cfd58f5372d72
Soumil Shah <s.shah@jobtarget.com>
2022-10-29 14:08:50 -0400

    added examples

--- README.md
+++ README.md
@@ -1,2 +1,87 @@
-# python-aurora-postgressql-
-Learn how to Connect|insert|Read| Write Bulk Data from Aurora Postgres using Python psycopg2
+## Learn how to Connect|insert|Read| Write Bulk Data into Aurora Postgres using Python psycopg2
+
+# How to use 
+### Steps
+
+##### Step1 : Install Library 
+```
+pip3 install -r requirements.txt
+```
+##### Step2 : Use Connector Object or Create instance 
+
+```
+helper = DatabaseAurora(
+    data_base_settings=Settings(
+        port=os.getenv("AURORA_DB_PORT"),
+        server=os.getenv("AURORA_DB_SERVER"),
+        username=os.getenv("AURORA_DB_UID"),
+        password=os.getenv("AURORA_DB_PWD"),
+        database_name=os.getenv("AURORA_DB_DATABASE"),
+    )
+)
+```
+
+##### OR
+```
+helper = DatabaseAurora(
+    data_base_settings=Settings(
+        port="XXXXXX",
+        server="XXXXXX",
+        username="XXXXXX",
+        password="XXXXXX",
+        database_name="XXXXXX",
+    )
+)
+```
+##### Step3 :Access All Methods in Class Easily 
+
+##### Create table 
+```
+    query = """
+                CREATE TABLE IF NOT EXISTS public.users
+                (
+                    first_name character varying(256) COLLATE pg_catalog."default",
+                    last_name character varying(256) COLLATE pg_catalog."default",
+                    address character varying(256) COLLATE pg_catalog."default",
+                    text character varying(256) COLLATE pg_catalog."default",
+                    id character varying(256) COLLATE pg_catalog."default",
+                    city character varying(256) COLLATE pg_catalog."default",
+                    state character varying(256) COLLATE pg_catalog."default"
+                )
+                
+                TABLESPACE pg_default;
+                
+                ALTER TABLE IF EXISTS public.users
+                    OWNER to postgres;
+    """
+    logger.logger.info("Creating Tables")
+    response = helper.execute(query=query, data=None)
+```
+
+##### Bulk Inserts 
+```
+    data = [
+        {'first_name': 'Jasmin', 'last_name': 'Cox', 'address': '21610 Guzman Burg\nNorth Daniel, CT 53547', 'text': 'International job so perhaps decide whether see. Water prove house direction may off. Which hotel rule guy base thing.', 'id': 'ffe6fb75-a0a5-4407-b35a-bf183d6940fc', 'city': 'North Sheilashire', 'state': 'Missouri'},
+        {'first_name': 'Michael', 'last_name': 'Hubbard', 'address': '462 Kevin Harbor Apt. 593\nCodyfort, NH 20624', 'text': 'Dark these partner hope. Article doctor fine room star. Why without different control.\nForget into apply forget project when. Put look also deep air.', 'id': '21a3d9b9-534a-481a-8c79-db71c8aeded2', 'city': 'West Paul', 'state': 'Alabama'}
+    ]
+    values = [tuple(item.values()) for item in data]
+    response = helper.insert_many(query=query, data=values)
+
+    
+```
+##### Read Data in Paginated Manner  
+
+```
+    response = helper.get_data_batch(query="""SELECT * FROM public.users""", batch_size=100)
+    while True:
+        try:
+            json_data = next(response)
+            print(json_data)
+        except Exception as e:
+            print("Complete Reading ")
+            break
+
+```
+##### Class Diagram 
+![image](https://user-images.githubusercontent.com/39345855/198846504-f36b0502-8578-4001-af46-f756de01e08c.png)
+
---
+++ helper.py
@@ -0,0 +1,204 @@
+try:
+    import uuid
+
+    from datetime import datetime
+    import os
+    import logging
+
+    from functools import wraps
+    from enum import Enum
+    from abc import ABC, abstractmethod
+    from logging import StreamHandler
+
+    import psycopg2
+
+    import psycopg2.extras as extras
+    from faker import Faker
+
+except Exception as e:
+    raise Exception("Error : {}".format(e))
+
+DATA = {
+    "AURORA_DB_SERVER": "XXXXXXXXXXXom",
+    "AURORA_DB_PORT": "5432",
+    "AURORA_DB_UID": "XXXXXXXX",
+    "AURORA_DB_PWD": "XXXX",
+    "AURORA_DB_DATABASE": "XXX",
+}
+
+for key, value in DATA.items():
+    os.environ[key] = value
+
+
+class Logging(object):
+    def __init__(self):
+        format = "[%(asctime)s] %(name)s %(levelname)s %(message)s"
+        # Logs to file
+        logging.basicConfig(
+            filename="logfile",
+            filemode="a",
+            format=format,
+            level=logging.INFO,
+        )
+        self.logger = logging.getLogger("python")
+        formatter = logging.Formatter(
+            format,
+        )
+        # Logs to Console
+        ch = logging.StreamHandler()
+        ch.setLevel(logging.INFO)
+        ch.setFormatter(formatter)
+        self.logger.addHandler(ch)
+
+
+global logger
+logger = Logging()
+
+
+def error_handling_with_logging(argument=None):
+    def real_decorator(function):
+        @wraps(function)
+        def wrapper(self, *args, **kwargs):
+            function_name = function.__name__
+            response = None
+            try:
+                if kwargs == {}:
+                    response = function(self)
+                else:
+                    response = function(self, **kwargs)
+            except Exception as e:
+                response = {
+                    "status": -1,
+                    "error": {"message": str(e), "function_name": function.__name__},
+                }
+                logger.logger.info(response)
+            return response
+
+        return wrapper
+
+    return real_decorator
+
+
+class Settings(object):
+    """settings class"""
+
+    def __init__(
+            self,
+            port="",
+            server="",
+            username="",
+            password="",
+            timeout=100,
+            database_name="",
+            **kwargs,
+    ):
+        self.port = port
+        self.server = server
+        self.username = username
+        self.password = password
+        self.timeout = timeout
+        self.database_name = database_name
+
+
+class DatabaseInterface(ABC):
+    @abstractmethod
+    def get_data(self, query):
+        """
+        For given query fetch the data
+        :param query: Str
+        :return: Dict
+        """
+
+    def execute(self, query, value):
+        """
+        Inserts data into SQL Server
+        :param query:  Str
+        :return: Dict
+        """
+
+    def insert_many(self, query, data):
+        """
+        Insert Many items into database
+        :param query: str
+        :param data: tuple
+        :return: Dict
+        """
+
+    def get_data_batch(self, batch_size=10, query=""):
+        """
+        Gets data into batches
+        :param batch_size: INT
+        :param query: STR
+        :return: DICT
+        """
+
+
+class DatabaseAurora(DatabaseInterface):
+    """Aurora database class"""
+
+    def __init__(self, data_base_settings):
+        self.data_base_settings = data_base_settings
+        self.client = psycopg2.connect(
+            host=self.data_base_settings.server,
+            port=self.data_base_settings.port,
+            database=self.data_base_settings.database_name,
+            user=self.data_base_settings.username,
+            password=self.data_base_settings.password,
+        )
+
+    @error_handling_with_logging()
+    def get_data(self, query):
+        self.query = query
+        cursor = self.client.cursor()
+        cursor.execute(self.query)
+        result = cursor.fetchall()
+        columns = [column[0] for column in cursor.description]
+        data = [dict(zip(columns, item)) for item in result]
+        cursor.close()
+        _ = {"statusCode": 200, "data": data}
+
+        return _
+
+    @error_handling_with_logging()
+    def execute(self, query, data):
+        self.query = query
+        cursor = self.client.cursor()
+        cursor.execute(self.query, data)
+        self.client.commit()
+        cursor.close()
+        return {"statusCode": 200, "data": True}
+
+    @error_handling_with_logging()
+    def get_data_batch(self, batch_size=10, query=""):
+        self.query = query
+        cursor = self.client.cursor()
+        cursor.execute(self.query)
+        columns = [column[0] for column in cursor.description]
+        while True:
+            result = cursor.fetchmany(batch_size)
+            if not result:
+                break
+            else:
+                items = [dict(zip(columns, data)) for data in result]
+                yield items
+
+    @error_handling_with_logging()
+    def insert_many(self, query, data):
+        self.query = query
+        cursor = self.client.cursor()
+        extras.execute_batch(cursor, self.query, data)
+        self.client.commit()
+        cursor.close()
+        return {"statusCode": 200, "data": True}
+
+
+class Connector(Enum):
+    DB_AURORA_PSYCOPG2 = DatabaseAurora(
+        data_base_settings=Settings(
+            port=os.getenv("AURORA_DB_PORT"),
+            server=os.getenv("AURORA_DB_SERVER"),
+            username=os.getenv("AURORA_DB_UID"),
+            password=os.getenv("AURORA_DB_PWD"),
+            database_name=os.getenv("AURORA_DB_DATABASE"),
+        )
+    )
